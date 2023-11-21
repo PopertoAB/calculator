c2195ba952631dadd90ddf40dd490511b679912d
Manuel Cuevas <scuevas@to-learn-cd.c.customer-repro-405322.internal>
2023-11-21 02:08:30 +0000

    Introducing error

--- src/main/java/com/popertoab/calculator/CalculatorApplication.java
+++ src/main/java/com/popertoab/calculator/CalculatorApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CalculatorApplication {
 
+    private final string name="Manuel";
+
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
 	}
