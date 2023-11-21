88af82b269afb41e12636370a2a549e75b8f0b61
Manuel Cuevas <scuevas@to-learn-cd.c.customer-repro-405322.internal>
2023-11-21 02:29:03 +0000

    Fixing introduced issue

--- src/main/java/com/popertoab/calculator/CalculatorApplication.java
+++ src/main/java/com/popertoab/calculator/CalculatorApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CalculatorApplication {
 
-    private static final String name="Manuel";
-
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
 	}
