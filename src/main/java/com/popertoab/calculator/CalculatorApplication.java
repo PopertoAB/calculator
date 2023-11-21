203c55a1cbca90938e83f02bfb1e7d02aa879df0
Manuel Cuevas <scuevas@to-learn-cd.c.customer-repro-405322.internal>
2023-11-21 02:24:48 +0000

    Fixing the introduced error

--- src/main/java/com/popertoab/calculator/CalculatorApplication.java
+++ src/main/java/com/popertoab/calculator/CalculatorApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CalculatorApplication {
 
-    private final string name="Manuel";
+    private static final String name="Manuel";
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
